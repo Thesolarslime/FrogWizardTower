9905934f71658399822807e4609b4807629cd8f4
SB321462 <SB321462@falmouth.ac.uk>
2026-09-03 15:01:39 +0100

    [backGround] added moving clouds

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+    
+    [SerializeField] GameObject BackGround;
+
+    public float BackgroundSpawnTimer = 0;
+    private void Awake()
+    {
+        // if there is a version of this which isnt this, delete this 
+        if (instance != null && instance != this)
+        { 
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    Vector3 BackGroundSpawnLocation;
+    int RandomSpawn;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RandomSpawn = Random.Range(50, 70);
+
+        BackGroundSpawnLocation = new Vector3(RandomSpawn, 6.4f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BackgroundSpawnTimer += Time.deltaTime;
+
+        if (BackgroundSpawnTimer > 10)
+        {
+            RandomSpawn = Random.Range(45, 70);
+            Instantiate(BackGround, BackGroundSpawnLocation, Quaternion.identity);
+            BackgroundSpawnTimer = 0;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PanThingToLeft.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PanThingToLeft : MonoBehaviour
+{
+    public float PanSpeed = -0.01f;
+    public float DestroySelfTime = 50f;
+    float elapsedTime = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PanSpeed = -0.01f;
+        DestroySelfTime = 50f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(PanSpeed,0,0);
+
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > DestroySelfTime)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+    }
+}
