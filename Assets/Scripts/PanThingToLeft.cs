ebf4331c27e3166edbc41605c00b28736fad937a
SB321462 <SB321462@falmouth.ac.uk>
2026-09-11 16:18:09 +0100

    reverting project back

--- Assets/Scripts/PanThingToLeft.cs
+++ Assets/Scripts/PanThingToLeft.cs
@@ -2,24 +2,21 @@ using UnityEngine;
 
 public class PanThingToLeft : MonoBehaviour
 {
-    public float PanSpeed = -0.01f;
-    public float DestroySelfTime = 50f;
-    float elapsedTime = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float PanSpeed = -1.3f;
+   
     void Start()
     {
-        PanSpeed = Random.Range(-0.002f, -0.007f);
+        //PanSpeed = Random.Range(-2f, -1f);
         transform.position = transform.position - new Vector3(0, Random.Range(-3, 0.6f), 0);
-        DestroySelfTime = 50f;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position += new Vector3(PanSpeed,0,0);
-
-        elapsedTime += Time.deltaTime;
-        if (elapsedTime > DestroySelfTime)
+        gameObject.transform.position += new Vector3(PanSpeed * Time.deltaTime,0,0);
+      
+        if (transform.position.x <= -65)
         {
             Destroy(this.gameObject);
         }
